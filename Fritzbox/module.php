c135bdc63f2da335f1e4ed575617efd6b65cb47a
Xaver Bauer <x.bauer@tier-freunde.net>
2016-01-23 18:57:56 +0100

    V7

--- Fritzbox/module.php
+++ Fritzbox/module.php
@@ -108,7 +108,7 @@ class fritzbox extends RpcBaseModule {
 			// Check If local Phonebook Exist , create it when not
 			$file=$this->ReadPropertyString('PhonebookFile');
 			if(strpos($file,'/')===false)
-				$file=self::MkDir($this->HONEBOOK_PATH).$file;
+				$file=self::MkDir($this->HOMEBOOK_PATH).$file;
 						
 			
 			if(!file_exists($file))
